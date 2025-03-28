67c9af8d7e51895f8f22b679bec385d8d437ccd6
Edmund <eyu1@ualberta.ca>
2025-03-28 14:53:24 -0600

    update copilot keybinds

--- init.lua
+++ init.lua
@@ -7,37 +7,48 @@ vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
 -- [[ Setting Options ]]
-vim.opt.number = true
-vim.opt.mouse = 'a'
-vim.opt.showmode = false
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.breakindent = true
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = 'split'
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
-vim.opt.confirm = true
-vim.opt.relativenumber = true
+-- See `:help vim.opt`
+vim.opt.number = true -- Print the line number in front of each line
+vim.opt.mouse = 'a' -- Enable mouse support in all modes
+vim.opt.showmode = false -- Don't show the mode, since it's in the status line
+vim.opt.clipboard = 'unnamedplus' -- Sync with system clipboard
+vim.opt.breakindent = true -- Enable break indent
+vim.opt.undofile = true -- Save undo history
+vim.opt.ignorecase = true -- Ignore case when searching
+vim.opt.smartcase = true -- Don't ignore case if search pattern contains uppercase letters
+vim.opt.signcolumn = 'yes' -- Always show the signcolumn, otherwise it would shift the text
+vim.opt.updatetime = 250 -- Decrease update time
+vim.opt.timeoutlen = 300 -- Decrease mapped sequence wait time
+vim.opt.splitright = true -- Place new vertical splits to the right
+vim.opt.splitbelow = true -- Place new horizontal splits below
+vim.opt.list = true -- Show invisible characters (tabs, spaces, trailing spaces, etc.)
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' } -- Set characters for list option
+vim.opt.inccommand = 'split' -- Preview substitutions live, as you type
+vim.opt.cursorline = true -- Highlight the current line
+vim.opt.scrolloff = 10 -- Keep 10 lines visible above/below the cursor when scrolling
+vim.opt.confirm = true -- Ask for confirmation when closing buffers with unsaved changes
+vim.opt.relativenumber = true -- Show relative line numbers
 
 -- [[ Basic Keymaps ]]
+-- See `:help vim.keymap.set()`
+-- Clear search highlights
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlights' })
+-- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- Exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- Window navigation
 vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move focus to left window' })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move focus to right window' })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move focus to lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move focus to upper window' })
 
+-- NvimTree Keymaps (Restored)
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle file [E]xplorer (NvimTree)' })
+vim.keymap.set('n', '<leader>o', '<cmd>NvimTreeFocus<CR>', { desc = '[O]pen file explorer focus (NvimTree)' })
+
 -- [[ Basic Autocommands ]]
+-- See `:help autocmd`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking text',
   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
@@ -46,7 +57,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- [[ Install lazy.nvim ]]
+-- [[ Install `lazy.nvim` plugin manager ]]
+-- See `:help lazy.nvim`
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', 'https://github.com/folke/lazy.nvim.git', lazypath }
@@ -55,8 +67,11 @@ vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure Plugins ]]
 require('lazy').setup {
-  spec = { -- Explicitly define spec to satisfy LSP
+  spec = { -- Define the list of plugins to install
+    -- Detect tabstop and shiftwidth automatically
     { 'tpope/vim-sleuth' },
+
+    -- Git integration signs in the sign column
     {
       'lewis6991/gitsigns.nvim',
       opts = {
@@ -69,12 +84,14 @@ require('lazy').setup {
         },
       },
     },
+
+    -- Keybinding hints popup
     {
       'folke/which-key.nvim',
-      event = 'VeryLazy',
+      event = 'VeryLazy', -- Load very late
       opts = {
-        delay = 0,
-        icons = { mappings = vim.g.have_nerd_font },
+        delay = 0, -- Show popup immediately
+        icons = { mappings = vim.g.have_nerd_font }, -- Use nerd font icons if available
         spec = {
           { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
           { '<leader>d', group = '[D]ocument' },
@@ -83,9 +100,13 @@ require('lazy').setup {
           { '<leader>w', group = '[W]orkspace' },
           { '<leader>t', group = '[T]oggle' },
           { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+          { '<leader>e', group = '[E]xplorer (Tree)' }, -- Restored description
+          { '<leader>o', group = '[O]pen Explorer (Tree)' }, -- Restored description
         },
       },
     },
+
+    -- Fuzzy Finder (files, lsp, etc)
     {
       'nvim-telescope/telescope.nvim',
       branch = '0.1.x',
@@ -131,7 +152,11 @@ require('lazy').setup {
         end, { desc = '[S]earch [N]eovim files' })
       end,
     },
+
+    -- LSP framework for Neovim lua development
     { 'folke/lazydev.nvim', ft = 'lua', opts = { library = { { path = '${3rd}/luv/library', words = { 'vim%.uv' } } } } },
+
+    -- LSP Configuration & Plugins
     {
       'neovim/nvim-lspconfig',
       dependencies = {
@@ -161,16 +186,14 @@ require('lazy').setup {
             local client = vim.lsp.get_client_by_id(event.data.client_id)
             if client and client.server_capabilities.documentHighlightProvider then
               local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
-              vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-                buffer = event.buf,
-                group = highlight_augroup,
-                callback = vim.lsp.buf.document_highlight,
-              })
-              vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-                buffer = event.buf,
-                group = highlight_augroup,
-                callback = vim.lsp.buf.clear_references,
-              })
+              vim.api.nvim_create_autocmd(
+                { 'CursorHold', 'CursorHoldI' },
+                { buffer = event.buf, group = highlight_augroup, callback = vim.lsp.buf.document_highlight }
+              )
+              vim.api.nvim_create_autocmd(
+                { 'CursorMoved', 'CursorMovedI' },
+                { buffer = event.buf, group = highlight_augroup, callback = vim.lsp.buf.clear_references }
+              )
               vim.api.nvim_create_autocmd('LspDetach', {
                 group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
                 callback = function(event2)
@@ -201,16 +224,7 @@ require('lazy').setup {
           virtual_text = { source = 'if_many', spacing = 2 },
         }
         local capabilities = vim.tbl_deep_extend('force', vim.lsp.protocol.make_client_capabilities(), require('cmp_nvim_lsp').default_capabilities())
-        local servers = {
-          lua_ls = {
-            settings = {
-              Lua = {
-                completion = { callSnippet = 'Replace' },
-                diagnostics = { globals = { 'vim' } },
-              },
-            },
-          },
-        }
+        local servers = { lua_ls = { settings = { Lua = { completion = { callSnippet = 'Replace' }, diagnostics = { globals = { 'vim' } } } } } }
         require('mason').setup()
         local ensure_installed = vim.tbl_keys(servers or {})
         vim.list_extend(ensure_installed, { 'stylua' })
@@ -226,6 +240,8 @@ require('lazy').setup {
         }
       end,
     },
+
+    -- Formatting
     {
       'stevearc/conform.nvim',
       event = { 'BufWritePre' },
@@ -248,6 +264,8 @@ require('lazy').setup {
         formatters_by_ft = { lua = { 'stylua' } },
       },
     },
+
+    -- Autocompletion
     {
       'hrsh7th/nvim-cmp',
       event = 'InsertEnter',
@@ -297,29 +315,44 @@ require('lazy').setup {
         }
       end,
     },
+
+    -- Colorscheme
     {
       'ellisonleao/gruvbox.nvim',
       priority = 1000,
-      opts = {
-        dim_inactive = false, -- Explicitly set required field
-        transparent_mode = false, -- Explicitly set required field
-        styles = { comments = { italic = false } },
-      },
+      opts = { dim_inactive = false, transparent_mode = false, styles = { comments = { italic = false } } },
       config = function(_, opts)
         require('gruvbox').setup(opts)
         vim.cmd.colorscheme 'gruvbox'
       end,
     },
-    { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+
+    -- Highlight TODOs, FIXMEs, etc.
+    {
+      'folke/todo-comments.nvim',
+      event = 'VimEnter',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      opts = { signs = false },
+    },
+
+    -- Collection of minimal useful plugins (mini.nvim)
     {
       'echasnovski/mini.nvim',
       config = function()
-        require('mini.ai').setup { n_lines = 500 }
-        require('mini.surround').setup()
-        -- Removed the incorrect line: require('mini.statusLordship('nvim-treesitter/nvim-treesitter')
-        require('mini.statusline').setup { use_icons = vim.g.have_nerd_font }
+        -- Enable required modules here
+        require('mini.ai').setup { n_lines = 500 } -- Enhanced textobjects (`a`, `i`)
+        require('mini.surround').setup() -- Add/delete/change surroundings (brackets, quotes, etc.)
+        require('mini.statusline').setup { use_icons = vim.g.have_nerd_font } -- Minimal statusline
+
+        -- mini.files setup has been removed.
+
+        -- To enable other modules, add their setup calls here, e.g.:
+        -- require('mini.pairs').setup() -- Autopairs
+        -- require('mini.comment').setup() -- Commenting tool
       end,
     },
+
+    -- Treesitter Parser & Syntax Highlighting
     {
       'nvim-treesitter/nvim-treesitter',
       build = ':TSUpdate',
@@ -329,37 +362,94 @@ require('lazy').setup {
           auto_install = true,
           highlight = { enable = true, additional_vim_regex_highlighting = { 'ruby' } },
           indent = { enable = true, disable = { 'ruby' } },
-        } -- Added missing closing brace for setup table
+        }
       end,
     },
-    -- Add GitHub Copilot plugin here
+
+    -- NvimTree (File Explorer - Primary)
     {
-      'zbirenbaum/copilot.lua',
-      cmd = 'Copilot', -- Lazy-load on :Copilot command
-      event = 'InsertEnter', -- Load when entering insert mode
+      'nvim-tree/nvim-tree.lua',
+      version = '*', -- Use the latest stable release
+      dependencies = {
+        'nvim-tree/nvim-web-devicons', -- Dependency for icons
+      },
+      cmd = { 'NvimTreeToggle', 'NvimTreeOpen', 'NvimTreeFindFileToggle', 'NvimTreeFocus' }, -- Lazy-load triggers
       config = function()
-        require('copilot').setup({
-          suggestion = {
-            enabled = true,
-            auto_trigger = true, -- Automatically show suggestions as you type
-            debounce = 75, -- Debounce time in ms
-            keymap = {
-              accept = '<M-]>', -- Alt+] to accept suggestion
-              accept_word = false, -- Disable accept by word
-              accept_line = false, -- Disable accept by line
-              next = '<M-}>', -- Alt+} to go to next suggestion
-              prev = '<M-{>', -- Alt+{ to go to previous suggestion
-              dismiss = '<C-]>', -- Ctrl+] to dismiss suggestion
+        require('nvim-tree').setup {
+          disable_netrw = true, -- Disable netrw
+          hijack_netrw = true, -- Let nvim-tree handle netrw buffers
+          diagnostics = {
+            enable = true,
+            icons = {
+              hint = vim.g.have_nerd_font and '󰌶' or 'H',
+              info = vim.g.have_nerd_font and '󰋽' or 'I',
+              warning = vim.g.have_nerd_font and '󰀪' or 'W',
+              error = vim.g.have_nerd_font and '󰅚' or 'E',
             },
           },
-          panel = { enabled = false }, -- Disable the Copilot panel (optional)
-          filetypes = {
-            ['*'] = true, -- Enable Copilot for all filetypes
+          view = {
+            width = 35, -- Width of the tree window
           },
-        })
+          renderer = {
+            group_empty = true, -- Show empty folders
+            icons = {
+              glyphs = {
+                default = vim.g.have_nerd_font and '󰈚' or 'F',
+                symlink = vim.g.have_nerd_font and '󰌹' or '->',
+                folder = {
+                  arrow_closed = vim.g.have_nerd_font and '' or '>',
+                  arrow_open = vim.g.have_nerd_font and '' or 'v',
+                  default = vim.g.have_nerd_font and '' or 'D',
+                  open = vim.g.have_nerd_font and '' or 'O',
+                  empty = vim.g.have_nerd_font and '󰜌' or 'E',
+                  empty_open = vim.g.have_nerd_font and '󰜌' or 'EO',
+                  symlink = vim.g.have_nerd_font and '󰉒' or '->D',
+                  symlink_open = vim.g.have_nerd_font and '󰉒' or '->O',
+                },
+                git = {
+                  unstaged = vim.g.have_nerd_font and '󰄱' or '!',
+                  staged = vim.g.have_nerd_font and '󰄱' or '+',
+                  unmerged = vim.g.have_nerd_font and '' or 'U',
+                  renamed = vim.g.have_nerd_font and '󰑕' or 'R',
+                  untracked = vim.g.have_nerd_font and '󰐕' or '?',
+                  deleted = vim.g.have_nerd_font and '󰍴' or '-',
+                  ignored = vim.g.have_nerd_font and '◌' or '.',
+                },
+              },
+            },
+          },
+        }
       end,
     },
-  },
-}
+
+    -- GitHub Copilot Integration
+    {
+      'zbirenbaum/copilot.lua',
+      cmd = 'Copilot',
+      event = 'InsertEnter',
+      config = function()
+        require('copilot').setup {
+          suggestion = {
+            enabled = true,
+            auto_trigger = true,
+            debounce = 75,
+            -- *** Alternative 1 Keymap Applied Below ***
+            keymap = {
+              accept = '<C-;>', -- Accept suggestion (Control + Semicolon)
+              accept_word = false, -- Keep disabled or map similarly if needed
+              accept_line = false, -- Keep disabled or map similarly if needed
+              next = '<C-j>', -- Next suggestion (Control + j) - like moving down
+              prev = '<C-k>', -- Previous suggestion (Control + k) - like moving up
+              dismiss = '<C-,>', -- Dismiss suggestion (Control + Comma) - near semicolon
+            },
+            -- *** End of Keymap Changes ***
+          },
+          panel = { enabled = false },
+          filetypes = { ['*'] = true },
+        }
+      end,
+    },
+  }, -- End of spec table
+} -- End of lazy.setup
 
 -- vim: ts=2 sts=2 sw=2 et
